b4ca617f833cfe7adfa278665f4db6e2befc22b0
Tom <tomburgess24@gmail.com>
2024-06-09 12:46:17 +0100

    encryptor and decryptor and example files

---
+++ shadowcaste.py
@@ -0,0 +1,23 @@
+import io
+
+# this is called shadowcaste
+
+a = io.open("text2shadow.txt", "r")
+filetext = a.read()
+charlist = []
+
+for x in filetext:
+	if not x in charlist:
+		charlist.append(x)
+
+length = len(charlist)
+
+def index(listchars, char):
+	return len(listchars) - (listchars.index(char) + 1)
+
+out = ""
+for character in filetext:
+	newchar = charlist[index(charlist, character)]
+	out += newchar
+
+print(out)
